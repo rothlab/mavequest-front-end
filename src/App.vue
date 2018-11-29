74e6ccaf0f4630859a91ea0020cc0780962dc1b5
Kevin Kuang <kvnkuang@users.noreply.github.com>
2018-11-28 19:02:33 -0500

    Added search bar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+   <nav class="navbar has-shadow">
+        <div class="container">
+          <div class="navbar-brand"><a class="navbar-item" href="../">
+          <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox"/></a>
+        </div>
+      </div>
+    </nav>
+
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Search Database</h1>
+          <SearchBar></SearchBar>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from "./components/SearchBar";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    SearchBar
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="search-bar">
+        <b-field>
+            <b-taginput
+              size="is-medium"
+              icon="search"
+              placeholder="Search with Gene Symbol, Entrez ID or Ensembl ID">
+            </b-taginput>
+        </b-field>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+//   props: {
+//     msg: String
+//   }
+}
+</script>

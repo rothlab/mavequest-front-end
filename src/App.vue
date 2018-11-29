65c3c42d0482a8d0f70c5c2b4926ce0a5b47c4b7
Kevin Kuang <kvnkuang@users.noreply.github.com>
2018-11-28 23:39:19 -0500

    Implemented the search page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-
-   <nav class="navbar has-shadow">
+    <section class="hero is-info is-bold is-fullheight">
+      <div class="hero-head">
+        <!-- Add navigation bar -->
         <div class="container">
-          <div class="navbar-brand"><a class="navbar-item" href="../">
-          <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox"/></a>
+          <header class="navbar">
+            <div class="navbar-brand">
+              <a class="navbar-item">Assay & Phenotype Database</a>
+              <span class="navbar-burger burger" data-target="navbarMenuHero">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div id="navbarMenuHero" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item is-active">Search</a>
+              <a class="navbar-item">Contact</a>
+            </div>
+            </div>
+          </header>
         </div>
       </div>
-    </nav>
 
-    <section class="hero is-medium is-primary is-bold">
       <div class="hero-body">
-        <div class="container">
+        <div class="container has-text-left">
+          <!-- Add search interface -->
           <h1 class="title">Search Database</h1>
+          <h2 class="subtitle">Search with Gene Symbol, Entrez ID or Ensembl ID</h2>
           <SearchBar></SearchBar>
         </div>
+      </div>
+
+      <div class="hero-foot">
       </div>
     </section>
   </div>
@@ -34,12 +50,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.navbar-burger {
+  color: white
 }
 </style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,20 +1,63 @@
 <template>
-    <div class="search-bar">
-        <b-field>
-            <b-taginput
-              size="is-medium"
-              icon="search"
-              placeholder="Search with Gene Symbol, Entrez ID or Ensembl ID">
-            </b-taginput>
-        </b-field>
+  <div class="search-bar">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <b-taginput size="is-medium"></b-taginput>
+        </div>
+        <div class="column is-narrow">
+          <button class="button is-medium">
+            <b-icon icon="search"></b-icon>
+            <span>Search</span>
+          </button>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column">
+          <b-collapse class="card search-option">
+            <div class="card-header">
+              <p class="card-header-title">Advanced Search</p>
+            </div>
+            <div class="card-content">
+                <section class="has-text-left search-option">
+                    <div class="field"><b-switch>Has Assay</b-switch></div>
+                    <div class="field"><b-switch>Has Disease Phenotype</b-switch></div>
+                </section>
+            </div>
+          </b-collapse>
+        </div>
+        <div class="column">
+          <div class="card search-option">
+            <header class="card-header">
+              <p class="card-header-title">Database Summary</p>
+            </header>
+            <div class="card-content">
+              <div class="content has-text-left is-medium">
+                <a>List all genes with potential assays</a>
+                <br>
+                <a>List all genes with disease phenotypes</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchBar',
-//   props: {
-//     msg: String
-//   }
-}
+  name: "SearchBar"
+  //   props: {
+  //     msg: String
+  //   }
+};
 </script>
+
+<style scoped>
+.search-option {
+    height: 100%;
+    align-items: center
+}
+</style>

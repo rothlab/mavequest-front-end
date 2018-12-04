6caff6eeeea90319e8b9187c5f12b86148333c32
Kevin Kuang <kvnkuang@users.noreply.github.com>
2018-12-04 16:28:50 -0500

    Added a floating "back-to-top" button.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,41 @@
 <template>
-  <section class="hero is-info">
-    <div class="hero-body">
-      <div class="container">
-        <div class="level">
-          <div class="level-left">
-            <h1 class="title" v-if="title">{{title}}</h1>
-            <h2 class="subtitle" v-if="subtitle">{{subtitle}}</h2>
-          </div>
-          <div class="level-right">
-            <SearchBar showButton></SearchBar>
+  <div>
+    <section class="hero is-info">
+      <div class="hero-body">
+        <div class="container">
+          <div class="level">
+            <div class="level-left">
+              <h1 class="title" v-if="title">{{title}}</h1>
+              <h2 class="subtitle" v-if="subtitle">{{subtitle}}</h2>
+            </div>
+            <div class="level-right">
+              <SearchBar showButton></SearchBar>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+    <!-- Add a back-to-top float button -->
+    <back-to-top visibleoffset="400">
+      <b-icon icon="fas fa-chevron-circle-up" size="is-large"></b-icon>
+    </back-to-top>
+  </div>
 </template>
 
 <script>
 import SearchBar from "@/components/SearchBar.vue";
 
 export default {
-    name: "Header",
-    components: {
-      SearchBar
-    },
-    props: {
-      title: String,
-      subtitle: String
-    }
-}
+  name: "Header",
+  components: {
+    SearchBar
+  },
+  props: {
+    title: String,
+    subtitle: String
+  }
+};
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+// Import Vue basics
 import Vue from 'vue'
 import router from './router'
+
+// Import UI framework related libraries
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'bulma-divider'
+
+// Import plugins
 import VueObserveVisibility from 'vue-observe-visibility'
+import BackToTop from 'vue-backtotop'
 
 // Make sure App is loaded last so that the styles are not overwritten
 import App from './App.vue'
@@ -14,7 +20,8 @@ Vue.config.productionTip = false
 Vue.use(Buefy, {
   defaultIconPack: 'fas'
 })
-.use(VueObserveVisibility);
+.use(VueObserveVisibility)
+.use(BackToTop);
 
 new Vue({
   router,
